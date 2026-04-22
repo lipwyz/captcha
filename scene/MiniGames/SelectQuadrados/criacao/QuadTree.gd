0698bfaf5cb6898805d97f1307b360f013ab1a78
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-04-22 15:04:35 -0300

    Comentarios no codigo da QuadTree
    adicionado comentários explicando as funções no código da QuadTree

--- scene/MiniGames/SelectQuadrados/criacao/QuadTree.gd
+++ scene/MiniGames/SelectQuadrados/criacao/QuadTree.gd
@@ -1,25 +1,33 @@
 class_name QuadTree
 extends RefCounted
 
+## QuadTree Node base da arvore, pai de todos
 var root: QuadTreeNode
 
+## Cria a arvore com dada profundidade de camadas (profundidade = 1 para somente o root e 4 filhos)
 func _init(profundidade: int) -> void:
 	root = QuadTreeNode.new(Vector2.ZERO, Vector2.ONE, profundidade)
 
+## Retorna os dados guardados em um nodo folha na dada posicao
 func get_dados(posicao: Vector2) -> Variant:
 	return root.get_dados(posicao)
 
+## Insere dados em um nodo folha na dada posicao
 func inserir(dados, posicao: Vector2) -> void:
 	root.inserir(dados, posicao)
 
+## Retorna uma lista de elementos [comeco, fim] : Array[Vector2]
+## 		para a nodo folha da arvore
 func get_all_dimensoes() -> Array:
 	return root.get_all_dimensoes(Vector2.ZERO, Vector2.ONE)
 
+# ------------------------------------------------------------------------------
+# QuadTree Node
+# ------------------------------------------------------------------------------
+
+## Classe para cada nodo da arvore
 class QuadTreeNode:
-	var top_esq : QuadTreeNode
-	var top_dir : QuadTreeNode
-	var bot_esq : QuadTreeNode
-	var bot_dir : QuadTreeNode
+	# Nodos filhos, organizados da seguinte maneira:
 	#  comeco --------------------------|
 	#  		|-------------|-------------|
 	#  		|---top_esq---|---top_dir---|
@@ -29,12 +37,20 @@ class QuadTreeNode:
 	#  		|---bot_esq---|---bot_dir---|
 	#  		|-------------|-------------|
 	#  		|---------------------------fim
+	var top_esq : QuadTreeNode
+	var top_dir : QuadTreeNode
+	var bot_esq : QuadTreeNode
+	var bot_dir : QuadTreeNode
 	
+	## Profundidade do nodo atual, quantidade de camadas de filhos (se for folha = 0)
 	var profundidade : int
+	## Posicao da metade do nodo, para calcular esq, dir, top e bot (null nos nodos folhas)
 	var pos_metade : Vector2
 	
-	var dados
+	## Dados que o nodo folha guarda (null nos pais)
+	var dados: Variant
 	
+	## Cria o nodo e seus filhos recursivamente
 	func _init(comeco : Vector2, fim : Vector2, _profundidade : int) -> void:
 		profundidade = _profundidade
 		
@@ -61,6 +77,7 @@ class QuadTreeNode:
 									fim,
 									_profundidade)
 	
+	## Insere _dados no nodo folha, com base na sua posicao
 	func inserir(_dados, posicao: Vector2) -> void:
 		if _is_nodo_folha():
 			# coloca os dados
@@ -73,6 +90,7 @@ class QuadTreeNode:
 		var nodo_filho := _get_nodo_filho(posicao)
 		nodo_filho.inserir(_dados, posicao)
 	
+	## Retorna a variavel dados do nodo
 	func get_dados(posicao: Vector2) -> Variant:
 		if _is_nodo_folha():
 			# retorne as flags
@@ -82,6 +100,8 @@ class QuadTreeNode:
 		var nodo_filho := _get_nodo_filho(posicao)
 		return nodo_filho.get_dados(posicao)
 	
+	## Retona uma lista com a dimensão de cada quadrado, somente dos nodos filhos
+	## Dimensao sendo comeco (topo esquerda) e fim (bottom direita) 
 	func get_all_dimensoes(comeco: Vector2, fim: Vector2) -> Array:
 		if _is_nodo_folha():
 			# retorne a lista com as posicoes [inicio, fim]
@@ -94,12 +114,12 @@ class QuadTreeNode:
 			+ bot_esq.get_all_dimensoes(Vector2(comeco.x, pos_metade.y), Vector2(pos_metade.x, fim.y))
 			+ bot_dir.get_all_dimensoes(pos_metade, fim)
 		)
-		
 	
-	## retorna se o nodo eh folha (nao tem filhos)
+	## Retorna verdadeiro se o nodo eh folha (nao tem filhos)
 	func _is_nodo_folha() -> bool:
 		return profundidade == 0
 	
+	## Retorna o nodo filho que contem a posicao -> top_esq, top_dir, bot_esq, bot_dir
 	func _get_nodo_filho(posicao: Vector2) -> QuadTreeNode:
 		# top
 		if posicao.y <= pos_metade.y:
