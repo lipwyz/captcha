824063c58d7c66684e8644d6591061d66d530b49
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-04-22 01:31:47 -0300

    Criado grid no minigame de Selecionar quadrados
    Criada temporariamente a grid para testar o funcionamento da Quad Tree no minigame de Selecionar os Quadrados

--- scene/MiniGames/SelectQuadrados/criacao/linhas.gd
+++ scene/MiniGames/SelectQuadrados/criacao/linhas.gd
@@ -1,3 +1,4 @@
+class_name LinhasSelectQuadrado
 extends Node2D
 
 ## Imagem que o jogador vai clicar
@@ -13,6 +14,7 @@ var pos_end   : Vector2
 func _ready() -> void:
 	criar_linhas()
 
+## Cria o quadrado roxo em volta da imagem
 func criar_linhas() -> void:
 	#var tamanho = Vector2(
 		#imagem_verificador.texture.get_width(), 
@@ -29,9 +31,49 @@ func criar_linhas() -> void:
 	line_2d.add_point(pos_start)
 	line_2d.add_point(pos_end)
 
+func desenhar_posicoes(lista: Array) -> void:
+	# tamanho da imagem (para converter de [0, 1] para o tam da imagem
+	var size_img := pos_end - pos_start
+	# para cada quadrado na lista
+	for quad: Array in lista:
+		# cria uma line2D para esse quadrado
+		var line := Line2D.new()
+		add_child(line)
+		line.width = 3
+		line.default_color = Color.from_hsv(randf(), 1.0, 1.0)
+		# pega os pontos de comeco e fim, e cria os 4 cantos
+		var pontos = _get_pontos_quadrado(quad[0], quad[1])
+		for p in pontos:
+			p = pos_start + (p * size_img)
+			line.add_point(p)
+
+# retorna os 4 cantos do quadrado, dado comeco (esq cima) e fim (direita baixo)
+func _get_pontos_quadrado(comeco : Vector2, fim : Vector2) -> Array:
+	return [
+		Vector2(comeco.x, comeco.y),	# top esq
+		Vector2(fim.x, comeco.y), 		# top dir
+		Vector2(fim.x, fim.y), 			# bot dir
+		Vector2(comeco.x, fim.y),		# bot esq
+		# repetido do primeiro, para fechar o quadrado
+		Vector2(comeco.x, comeco.y), 	# top esq
+	]
+
+# Clicar aumenta em 1 o valor que tem no quadrado
+@onready var mini_game_select_quadrados: Control = $"../.."
 func click(pos: Vector2) -> void:
 	
-	line_2d.add_point(pos)
+	#line_2d.add_point(pos)
+	
+	# pega o quadrado
+	var quad_tree : QuadTree = mini_game_select_quadrados.quad_tree
+	# converte a global_pos do mouse para o [0.0, 1.0] do quad tree
+	var pos_quad : Vector2 = (pos - pos_start) / (pos_end - pos_start)
+	# pega o valor, se nao tiver, valor = 1
+	var valor = quad_tree.get_dados(pos_quad)
+	if not valor: valor = 1
+	# printa o valor e insere +1 dps
+	print("Valor no quad: %d" % valor)
+	quad_tree.inserir(valor+1, pos_quad)
 
 func _processar_click(pos: Vector2) -> void:
 	# ignora se estiver fora da imagem
@@ -39,7 +81,7 @@ func _processar_click(pos: Vector2) -> void:
 	if pos.y > pos_end.y: return 
 	if pos.x < pos_start.x: return 
 	if pos.x > pos_end.x: return 
-	# se estiver dentro dda imagem
+	# se estiver dentro da imagem
 	click(pos)
 
 func _input(event: InputEvent) -> void:
--- scene/MiniGames/SelectQuadrados/criacao/mini_game_select_quadrados.gd
+++ scene/MiniGames/SelectQuadrados/criacao/mini_game_select_quadrados.gd
@@ -1,1 +1,11 @@
 extends ConteudoAba
+
+@export var quad_tree_profundidade : int = 3
+var quad_tree : QuadTree
+
+@onready var linhas: LinhasSelectQuadrado = $Mundo/Linhas
+
+func _ready() -> void:
+	quad_tree = QuadTree.new(quad_tree_profundidade)
+	await get_tree().create_timer(0.1).timeout
+	linhas.desenhar_posicoes(quad_tree.get_all_dimensoes())
