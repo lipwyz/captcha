9d2948e82a28d2d2a2402e98a4fb98fbe562c00f
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-04-22 23:08:55 -0300

    Criado QuadTreeSelecao para minigame selecao
    Ajustada QuadTree base com novos metodos
    Criada a QuadTreeSelecao especificamente para metodos de selecionar quadrados

--- scene/MiniGames/SelectQuadrados/criacao/QuadTree.gd
+++ scene/MiniGames/SelectQuadrados/criacao/QuadTree.gd
@@ -16,10 +16,18 @@ func get_dados(posicao: Vector2) -> Variant:
 func inserir_dados(dados, posicao: Vector2) -> void:
 	root.inserir_dados(dados, posicao)
 
-## Retorna uma lista de elementos [comeco, fim] : Array[Vector2]
-## 		para a nodo folha da arvore
 func get_all_dimensoes() -> Array:
-	return root.get_all_dimensoes(Vector2.ZERO, Vector2.ONE)
+	return root.get_all_dimensoes()
+
+## Retorna uma lista de elementos [comeco, fim] : Array[Vector2]
+## 		para cada nodo folha da arvore
+func get_folhas_dimensoes() -> Array[Vector2]:
+	return root.get_folhas_dimensoes()
+
+## Retorna uma lista de elementos [comeco, fim] : Array[Vector2]
+## 		para cada nodo folha da arvore
+func get_dimensoes_nodos_com_dados(dados_comparar: Variant) -> Array[Vector2]:
+	return root.get_dimensoes_nodos_com_dados(dados_comparar)
 
 # ------------------------------------------------------------------------------
 # QuadTree Node
@@ -47,12 +55,17 @@ class QuadTreeNode:
 	## Posicao da metade do nodo, para calcular esq, dir, top e bot (null nos nodos folhas)
 	var pos_metade : Vector2
 	
+	var pos_comeco : Vector2
+	var pos_fim : Vector2
+	
 	## Dados que o nodo folha guarda (null nos pais)
 	var dados: Variant
 	
 	## Cria o nodo e seus filhos recursivamente
 	func _init(comeco : Vector2, fim : Vector2, _profundidade : int) -> void:
 		profundidade = _profundidade
+		pos_comeco = comeco
+		pos_fim    = fim
 		
 		# se for nodo folha (sem filhos), pare aqui
 		if _profundidade == 0:
@@ -79,7 +92,7 @@ class QuadTreeNode:
 	
 	## Insere _dados no nodo folha, com base na sua posicao
 	func inserir_dados(_dados, posicao: Vector2) -> void:
-		if _is_nodo_folha():
+		if is_nodo_folha():
 			# coloca os dados
 			dados = _dados
 			# para a func
@@ -90,9 +103,21 @@ class QuadTreeNode:
 		var nodo_filho := _get_nodo_filho(posicao)
 		nodo_filho.inserir_dados(_dados, posicao)
 	
+	## Insere _dados no nodo e seus filhos
+	func inserir_dados_nodo_e_filhos(_dados) -> void:
+		if is_nodo_folha():
+			# coloca os dados
+			dados = _dados
+			# para a func
+			return
+			
+		# nodo nao eh folha, passar para os filhos
+		for filho in [top_esq, top_dir, bot_esq, bot_dir]:
+			filho.inserir_dados_nodo_e_filhos(_dados)
+	
 	## Retorna a variavel dados do nodo
 	func get_dados(posicao: Vector2) -> Variant:
-		if _is_nodo_folha():
+		if is_nodo_folha():
 			# retorne as flags
 			return dados
 			
@@ -100,30 +125,80 @@ class QuadTreeNode:
 		var nodo_filho := _get_nodo_filho(posicao)
 		return nodo_filho.get_dados(posicao)
 	
-	## Retona uma lista com a dimensão de cada quadrado, somente dos nodos filhos
+	## Retona uma lista com a dimensão de cada quadrado
 	## Dimensao sendo comeco (topo esquerda) e fim (bottom direita) 
-	func get_all_dimensoes(comeco: Vector2, fim: Vector2) -> Array:
-		if _is_nodo_folha():
-			# retorne a lista com as posicoes [inicio, fim]
-			return [[comeco, fim]]
+	func get_all_dimensoes() -> Array:
+		if is_nodo_folha():
+			# retorne as posicoes [inicio, fim]
+			return [pos_comeco, pos_fim, [] ]
 		
-		#se tem filhos, retorne a posicao de cada um dos 4
-		return (
-			top_esq.get_all_dimensoes(comeco, pos_metade)
-			+ top_dir.get_all_dimensoes(Vector2(pos_metade.x, comeco.y), Vector2(fim.x, pos_metade.y))
-			+ bot_esq.get_all_dimensoes(Vector2(comeco.x, pos_metade.y), Vector2(pos_metade.x, fim.y))
-			+ bot_dir.get_all_dimensoes(pos_metade, fim)
+		# se tem filhos
+		var dimensoes_filhos : Array = (
+			top_esq.get_all_dimensoes()
+			+ top_dir.get_all_dimensoes()
+			+ bot_esq.get_all_dimensoes()
+			+ bot_dir.get_all_dimensoes()
 		)
+		
+		return [pos_comeco, pos_fim, dimensoes_filhos]
+	
+	## Retona uma lista com a dimensão de cada quadrado, somente dos nodos filhos
+	## 	Dimensao sendo comeco (topo esquerda) e fim (bottom direita) 
+	func get_folhas_dimensoes() -> Array:
+		if is_nodo_folha():
+			return get_dimensoes_nodo()
+		
+		# se tem filhos, retorne a posicao de cada um dos 4
+		return (
+			top_esq.get_folhas_dimensoes()
+			+ top_dir.get_folhas_dimensoes()
+			+ bot_esq.get_folhas_dimensoes()
+			+ bot_dir.get_folhas_dimensoes()
+		)
+	
+	# retorne a lista com as posicoes [inicio, fim]
+	func get_dimensoes_nodo() -> Array[Array]:
+		return [[pos_comeco, pos_fim]]
+	
+	func get_dimensoes_filhos_nodos_com_dados(dados_comparar: Variant) -> Array:
+		# se o valor nao for igual, retorne o tamanho dos filhos
+		if dados != dados_comparar:
+			return (
+				top_esq.get_dimensoes_nodo()
+				+ top_dir.get_dimensoes_nodo()
+				+ bot_esq.get_dimensoes_nodo()
+				+ bot_dir.get_dimensoes_nodo()
+			)
+		# se o valor for igual
+		
+		# se chegar nos nodos folha, retorne a posicao deles
+		if is_nodo_folha():
+			return get_dimensoes_nodo()
+		
+		# dimensoes dos filhos
+		var dimensoes_filhos : Array = (
+			top_esq.get_dimensoes_filhos_nodos_com_dados(dados_comparar)
+			+ top_dir.get_dimensoes_filhos_nodos_com_dados(dados_comparar)
+			+ bot_esq.get_dimensoes_filhos_nodos_com_dados(dados_comparar)
+			+ bot_dir.get_dimensoes_filhos_nodos_com_dados(dados_comparar)
+		)
+		
+		#se nenhnum filho retornou nada, bote o valor deste nodo
+		if dimensoes_filhos.is_empty():
+			return [[pos_comeco, pos_fim]]
+		# se os filhos retornaram algo, retorne os valores deles
+		else:
+			return dimensoes_filhos
 	
 	## Retorna o nodo folha na dada posicao
 	func get_nodo_folha(posicao: Vector2) -> QuadTreeNode:
-		if _is_nodo_folha():
+		if is_nodo_folha():
 			return self
 		# se for pai -> chama get_nodo_folha no nodo filho da posicao correspondente
 		return _get_nodo_filho(posicao).get_nodo_folha(posicao)
 	
 	## Retorna verdadeiro se o nodo eh folha (nao tem filhos)
-	func _is_nodo_folha() -> bool:
+	func is_nodo_folha() -> bool:
 		return profundidade == 0
 	
 	## Retorna o nodo filho que contem a posicao -> top_esq, top_dir, bot_esq, bot_dir
---
+++ scene/MiniGames/SelectQuadrados/criacao/QuadTreeSelecao.gd
@@ -0,0 +1,54 @@
+@tool
+class_name QuadTreeSelecao
+extends QuadTree
+
+const FLAG_CLICK 	:= 1
+const FLAG_CORRETO 	:= 2
+const FLAG_SHOW 	:= 4
+
+func _init(_profundidade: int) -> void:
+	super(_profundidade)
+	# zera as flags em todos os nodos
+	const flags_inicial : int = 0
+	_set_valor_inicial_todos_nodos(flags_inicial)
+	# mostra somente a primeira camada de quadrados (filhos diretos do root)
+	root.dados = FLAG_SHOW
+	for filho : QuadTreeNode in [root.top_esq, root.top_dir, root.bot_esq, root.bot_dir]:
+		filho.dados = FLAG_SHOW
+
+func mostrar_quadrados() -> Array[Array]:
+	return root.get_dimensoes_filhos_nodos_com_dados(FLAG_SHOW)
+
+func clicar(posicao: Vector2) -> void:
+	var nodo : QuadTreeNode = root.get_nodo_folha(posicao)
+	var flags :int = nodo.get_dados(posicao)
+	flags = flags | FLAG_CLICK
+	nodo.inserir_dados(flags, posicao)
+
+func set_valido(posicao: Vector2) -> void:
+	root.inserir_dados(FLAG_CORRETO, posicao)
+
+## Retorna os dados guardados em um nodo folha na dada posicao
+func get_dados(posicao: Vector2) -> Variant:
+	return root.get_dados(posicao)
+
+## Insere dados em um nodo folha na dada posicao
+func inserir(dados, posicao: Vector2) -> void:
+	root.inserir(dados, posicao)
+#
+### Retorna uma lista de elementos [comeco, fim] : Array[Vector2]
+### 		para a nodo folha da arvore
+#func get_all_dimensoes() -> Array:
+	#return root.get_all_dimensoes(Vector2.ZERO, Vector2.ONE)
+
+## Retorna os 4 cantos do quadrado, dado comeco (esq cima) e fim (direita baixo)
+func get_cantos_quadrado(comeco : Vector2, fim : Vector2) -> Array[Vector2]:
+	return [
+		Vector2(comeco.x, comeco.y),	# top esq
+		Vector2(fim.x, comeco.y), 		# top dir
+		Vector2(fim.x, fim.y), 			# bot dir
+		Vector2(comeco.x, fim.y),		# bot esq
+	]
+
+func _set_valor_inicial_todos_nodos(valor: int) -> void:
+	root.inserir_dados_nodo_e_filhos(valor)
