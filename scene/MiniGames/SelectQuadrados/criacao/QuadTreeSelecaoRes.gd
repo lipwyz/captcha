8f429c580b33dfbfee5c02dbfc53f231939839b9
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-04-22 23:11:20 -0300

    Criada a geração da quadTree pelo editor com tool

---
+++ scene/MiniGames/SelectQuadrados/criacao/QuadTreeSelecaoRes.gd
@@ -0,0 +1,6 @@
+class_name QuadTreeSelecaoRes
+extends Resource
+
+@export var profundidade: int
+
+@export var quadTree_positions : Array[Vector2]
--- scene/MiniGames/SelectQuadrados/criacao/mini_game_select_quadrados.gd
+++ scene/MiniGames/SelectQuadrados/criacao/mini_game_select_quadrados.gd
@@ -1,11 +1,11 @@
 extends ConteudoAba
 
-@export var quad_tree_profundidade : int = 3
-var quad_tree : QuadTree
+#@export var quad_tree_profundidade : int = 3
+#var quad_tree : QuadTreeSelecao
 
-@onready var linhas: LinhasSelectQuadrado = $Mundo/Linhas
+#@onready var linhas: LinhasSelectQuadrado = $Mundo/Linhas
 
-func _ready() -> void:
-	quad_tree = QuadTree.new(quad_tree_profundidade)
-	await get_tree().create_timer(0.1).timeout
-	linhas.desenhar_posicoes(quad_tree.get_all_dimensoes())
+#func _ready() -> void:
+	#quad_tree = QuadTreeSelecao.new(quad_tree_profundidade)
+	#await get_tree().create_timer(0.1).timeout
+	#linhas.desenhar_posicoes(quad_tree.get_all_dimensoes())
