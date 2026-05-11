1b60e69f3c284c5c3fed05dbe0e44f2691c863ae
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-05-11 17:39:23 -0300

    Organização: separado gerenciador de jogo
    Separado em gerenciador de mini games, e cut scene inicial, para melhor organização

--- scene/GerenciadorJogo/scripts/gerenciador_jogo.gd
+++ scene/GerenciadorJogo/scripts/gerenciador_jogo.gd
@@ -1,13 +1,6 @@
 class_name GerenciadorJogo
 extends Node
 
-@export_group("Game")
-@export var lista_mini_games: ListaMiniGamesRes
-
-@export_group("CutScene")
-@export var cena_cutscene: PackedScene
-@export var cena_cutscene_2: PackedScene
-
 @export_group("Abobe")
 @export var site_abobe_resource: MiniGameRes
 
@@ -16,6 +9,7 @@ extends Node
 @export var area_trabalho: AreaTrabalho
 
 @onready var gerenciador_mini_games: GerenciadorMiniGames = $GerenciadorMiniGames
+@onready var gerenciador_cut_scene_inicial: GerenciadorCutSceneInicial = $GerenciadorCutSceneInicial
 
 func _ready() -> void:
 	GerenciadorGlobal.gerenciador_jogo = self
@@ -25,51 +19,13 @@ func _ready() -> void:
 	navegador.fechar()
 	
 	await get_tree().process_frame
-	iniciar_cutscene()
+	gerenciador_cut_scene_inicial.iniciar_cutscene(navegador, area_trabalho)
 	
 	# TODO: sugestao colocar a animacao de intro antes de conectar os sinais
 	_conectar_sinais()
 	
 	# iniciar as abas de teste do navegador
 	iniciar_abas()
-
-
-func iniciar_cutscene() -> void:
-	var aba_cutscene := Aba.criar_aba(
-			"TwiXer",
-			false,
-			Aba.Estados.Idle,
-			"TwiXer/forYou")
-	navegador.add_aba(aba_cutscene, cena_cutscene)
-	
-	# TODO: inicar a cut scene
-	var nav_cont = navegador.navegador_conteudo
-	var teste_cutscene = nav_cont.conteudo_por_aba[aba_cutscene]
-	#if teste_cutscene is TestCutScene:
-		#aba_cutscene.clicada.connect(
-			#func():
-				#teste_cutscene.iniciar_cutscene()
-		#)
-	
-	# -------------
-	#var aba_cutscene_2 := Aba.criar_aba(
-			#"TwiXer 2",
-			#false,
-			#Aba.Estados.Idle,
-			#"TwiXer/forYou")
-	#navegador.add_aba(aba_cutscene_2, cena_cutscene_2)
-	#var teste_cutscene_2 = nav_cont.conteudo_por_aba[aba_cutscene_2]
-	#aba_cutscene_2.clicada.connect(func(): teste_cutscene_2.iniciar_cutscene() )
-	
-	# Ajustar a aba inicial
-	navegador.mudar_aba(aba_cutscene)
-	# TODO: mudar essa inicializacao
-	area_trabalho.click_navegador.connect(_iniciar_cutscene.bind(teste_cutscene))
-
-func _iniciar_cutscene(teste_cutscene: TestCutScene) -> void:
-	area_trabalho.click_navegador.disconnect(_iniciar_cutscene)
-	#await get_tree().create_timer(0.2).timeout
-	teste_cutscene.iniciar_cutscene()
 
 
 # TODO: funcoes temporarias
@@ -89,7 +45,7 @@ func criar_aba_inicial() -> void:
 # -----------------------------------------------------------------------------
 
 func iniciar_mini_games() -> void:
-	gerenciador_mini_games.iniciar_mini_games()
+	gerenciador_mini_games.iniciar_mini_games(navegador)
 
 #func iniciar_minigames() -> void:
 	#for mg_res: MiniGameRes in lista_mini_games.lista_faceis:
--- scene/GerenciadorJogo/scripts/gerenciador_mini_games.gd
+++ scene/GerenciadorJogo/scripts/gerenciador_mini_games.gd
@@ -1,7 +1,9 @@
 class_name GerenciadorMiniGames
 extends Node
 
-func iniciar_mini_games() -> void:
-	var p : GerenciadorJogo = get_parent()
-	var mg_res : MiniGameRes = p.lista_mini_games.lista_faceis.pick_random()
-	mg_res.navegador_add_mini_game(p.navegador)
+@export_group("Game")
+@export var lista_mini_games: ListaMiniGamesRes
+
+func iniciar_mini_games(navegador: Navegador) -> void:
+	var mg_res : MiniGameRes = lista_mini_games.lista_faceis.pick_random()
+	mg_res.navegador_add_mini_game(navegador)
