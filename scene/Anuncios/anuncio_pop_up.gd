f3310da77eb1700b98e9d62104312423cb62dcea
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-05-27 19:14:38 -0300

    docs: documentado o funcionamento do anuncio_pop_up
    Documentado as funções
    Removido prints
    Renomeado para manter a coerência

--- scene/Anuncios/anuncio_pop_up.gd
+++ scene/Anuncios/anuncio_pop_up.gd
@@ -3,50 +3,59 @@ extends Window
 
 signal fechado
 
-@export var botao_x : Button
+@export var botao_fechar : Button
 
+## tamanho maximo que a imagem / janela do anuncio deve ter
 var tamanho_max := Vector2.ONE
+## tamanho atual da imagem do anuncio
 var imagem_anuncio_size := Vector2.ONE
 
 func _ready() -> void:
-	botao_x.pressed.connect(func(): fechado.emit() )
+	# conectar os sinais
+	botao_fechar.pressed.connect(func(): fechado.emit() )
 
+## Inicia a janela de anuncio,
+##		imagem_anuncio um nodo do tipo ImagemAnuncio, contendo a imagem
+##		_tamanho_max a tamanho maximo que a imagem deve ter
 func iniciar(imagem_anuncio: ImagemAnuncio, _tamanho_max: Vector2) -> void:
-	add_child(imagem_anuncio)
 	tamanho_max = _tamanho_max
+	add_child(imagem_anuncio)
 	# resize a imagem
 	imagem_anuncio_size = _resize_tamanho_max(imagem_anuncio)
 	# resize a window
 	_resize_tamanho_window(imagem_anuncio_size)
 	# posiciona o X em alguma posicao da imagem
-	_ajustar_botao_x(imagem_anuncio_size)
+	_ajustar_botao_fechar(imagem_anuncio_size)
 
-func _ajustar_botao_x(imagem_anuncio_size: Vector2) -> void:
-	var max_size := Vector2(
-		imagem_anuncio_size.x - botao_x.size.x,
-		imagem_anuncio_size.y - botao_x.size.y
+## Ajusta a posicao do botao de fechar dentro da janela
+func _ajustar_botao_fechar(imagem_anuncio_size: Vector2) -> void:
+	# posicao minimas e maximas dentro da janela
+	var min_pos := Vector2.ZERO
+	var max_pos := Vector2(
+		imagem_anuncio_size.x - botao_fechar.size.x,
+		imagem_anuncio_size.y - botao_fechar.size.y
 	)
-	var pos_x := randf_range(0, max_size.x)
-	var pos_y := randf_range(0, max_size.y)
-	botao_x.position = Vector2(pos_x, pos_y)
-	botao_x.move_to_front()
+	# posicao aleatoria dentro do min e max
+	var pos_x := randf_range(0, max_pos.x)
+	var pos_y := randf_range(0, max_pos.y)
+	botao_fechar.position = Vector2(pos_x, pos_y)
+	# move para frente da janela para poder ser clicado
+	botao_fechar.move_to_front()
 
+## Ajusta o tamanho da janela para o mesmo da imagem
 func _resize_tamanho_window(imagem_anuncio_size: Vector2) -> void:
 	size = imagem_anuncio_size
 
+## Ajusta o tamanho maximo da imagem para ficar dentro de tamanho_max
+##		Retorna o tamanho da imagem apos sofrer o scale
 func _resize_tamanho_max(imagem_anuncio: ImagemAnuncio) -> Vector2:
 	var _size = imagem_anuncio.get_tamanho_sprite()
-	print("_size", _size)
 	if _size.y > tamanho_max.y:
 		imagem_anuncio.scale = Vector2.ONE * (tamanho_max.y / _size.y)
 		# atualiza o _size
 		_size = _size * (tamanho_max.y / _size.y)
-		#return _size
-		print("_size.y _size", _size)
 	if _size.x > tamanho_max.x:
 		imagem_anuncio.scale = Vector2.ONE * (tamanho_max.x / _size.x)
-		#_size = Vector2.ONE * (tamanho_max.x / _size.x)
+		# atualiza o _size
 		_size = _size * (tamanho_max.x / _size.x)
-		print("_size.x _size", _size)
-		#return _size
 	return _size
