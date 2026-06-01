56706924234685c4e702f70854551625cdd14148
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-01 17:22:25 -0300

    refactor: MG bario removido parar jogo
    Mini game bario retirado o parar jogo e corrigido o erro da vida perdida

--- scene/MiniGames/Mario/criacao/mini_game_mario.gd
+++ scene/MiniGames/Mario/criacao/mini_game_mario.gd
@@ -7,20 +7,9 @@ func _ready() -> void:
 	mundo.ganhou.connect(minigame_ganhar)
 	mundo.acabou_vidas.connect(minigame_perder)
 	mundo.morreu.connect(mostrar_ad)
-	# 
-	GerenciadorGlobal.todos_anuncios_fechados.connect(_sair_ad)
 
 func mostrar_ad() -> void:
 	# mostra o anuncio
 	minigame_errar()
-	call_deferred("_pausar_mundo")
-
-# pausa o mundo (processamento, animacoes, etc)
-func _pausar_mundo() -> void:
-	mundo.process_mode = Node.PROCESS_MODE_DISABLED
-
-func _sair_ad() -> void:
-	# volta o mundo (processamento, animacoes, etc)
-	mundo.process_mode = Node.PROCESS_MODE_INHERIT
 	# respawna o jogador
 	mundo.spawn()
