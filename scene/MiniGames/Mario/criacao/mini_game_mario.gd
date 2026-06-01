fbe3686eebd001f537a2fb8142bff2cbc10009ec
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-01 16:26:59 -0300

    feat: MG bario sinais atualizado
    Mini game bario world teve sinais atualizados
    Assim como sistema de respawn

--- scene/MiniGames/Mario/criacao/mini_game_mario.gd
+++ scene/MiniGames/Mario/criacao/mini_game_mario.gd
@@ -7,17 +7,19 @@ extends ConteudoAba
 func _ready() -> void:
 	ad_popup.hide()
 	# conecta os sinais de "fim de jogo"
-	mundo.perdeu.connect(mostrar_ad)
-	mundo.ganhou.connect(terminar)
-	# conecta o botao de sair do ad
-	button_skip.pressed.connect(_sair_ad)
+	mundo.ganhou.connect(minigame_ganhar)
+	mundo.morreu.connect(mostrar_ad)
+	# 
+	GerenciadorGlobal.todos_anuncios_fechados.connect(_sair_ad)
 
 func mostrar_ad() -> void:
-	ad_popup.show()
+	# mostra o anuncio
+	minigame_errar()
 	# pausa o mundo (processamento, animacoes, etc)
 	mundo.process_mode = Node.PROCESS_MODE_DISABLED
+	
 
 func _sair_ad() -> void:
-	ad_popup.hide()
-	# termina
-	terminar()
+	# volta o mundo (processamento, animacoes, etc)
+	mundo.process_mode = Node.PROCESS_MODE_INHERIT
+	mundo.spawn()
--- scene/MiniGames/Mario/criacao/mundo.gd
+++ scene/MiniGames/Mario/criacao/mundo.gd
@@ -1,21 +1,35 @@
 class_name MundoMario
 extends Node2D
 
-signal perdeu
 signal ganhou
+signal morreu
 
 @onready var coletavel: Area2D = $Coletavel
 @onready var zona_morte: Area2D = $ZonaMorte
 @onready var jogador: CharacterBody2D = $Jogador
+@onready var spawn_jogador: Node2D = $SpawnJogador
+
+var imortal: bool = false :
+	set(_imortal):
+		imortal = _imortal
+		await get_tree().create_timer(0.5).timeout
+		imortal = false
+
+func spawn() -> void:
+	imortal = true
+	jogador.velocity = Vector2.ZERO
+	jogador.global_position = spawn_jogador.global_position
 
 func _ready() -> void:
 	coletavel.body_entered.connect(_coletar)
 	zona_morte.body_entered.connect(_morrer)
 
 func _coletar(_body : Node) -> void:
-	await get_tree().create_timer(0.3).timeout
-	emit_signal("ganhou")
+	coletavel.hide()
+	await get_tree().create_timer(0.2).timeout
+	ganhou.emit()
 
 func _morrer(_body : Node) -> void:
-	await get_tree().create_timer(0.3).timeout
-	emit_signal("perdeu")
+	if imortal: return
+	
+	morreu.emit()
