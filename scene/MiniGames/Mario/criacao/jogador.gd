db853b497b4c014b7c5e9a0ebde1fc47a8811c1e
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-03 19:14:11 -0300

    feat: melhorado input do bario world
    Criado novo input map
    Aplicado alterações no Bario World

--- scene/MiniGames/Mario/criacao/jogador.gd
+++ scene/MiniGames/Mario/criacao/jogador.gd
@@ -17,12 +17,12 @@ func _physics_process(delta: float) -> void:
 			animated_sprite_2d.play("jumping")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("mov_up") and is_on_floor():
 		velocity.y = -JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("mov_left", "mov_right")
 	if not is_zero_approx(direction):
 		velocity.x = direction * SPEED
 		# vira pro lado que o jogador esta indo
