4ec197a7628aa546f3bc68d576b56922927b0af9
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-17 19:44:07 -0300

    feat: tool de criação de mini games
    Criada ferramenta de debug para criação de mini games

---
+++ _Debug/CriadorMiniGamesTool/criador_mini_games_tool.gd
@@ -0,0 +1,24 @@
+@tool
+class_name CriadorMiniGamesTool
+extends Node
+
+@export var tamanho_tela := Vector2(800, 600)
+
+@export_tool_button("Executar", "AcceptDialog") var botao_rodar = _run
+
+var window: Window
+
+const JANELA_CRIADOR_MINI_GAMES = preload("uid://gtsgth0k45ky")
+
+func _run() -> void:
+	# cria a janela
+	window = Window.new()
+	EditorInterface.popup_dialog(
+		window,
+		Rect2(Vector2.ONE*100, tamanho_tela)
+	)
+	# lida com fechar
+	window.close_requested.connect(func(): window.queue_free())
+	
+	# adiciona os elementos na janela
+	window.add_child(JANELA_CRIADOR_MINI_GAMES.instantiate())
