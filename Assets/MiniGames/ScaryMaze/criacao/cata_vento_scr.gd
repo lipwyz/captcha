c6e4e060eb553ca1ff3fe3df9a8c186b020a2404
Paulo B F Almeida <paulobfalmeida@gmail.com>
2026-06-18 00:52:24 -0300

    refactor: cata vendo usando delta para calcular velocidade
    Atualizado código do cata vento para utilizar delta do process no calculo da velocidade
    Alterada as velocidades para condizerem com o novo calculo

--- Assets/MiniGames/ScaryMaze/criacao/cata_vento_scr.gd
+++ Assets/MiniGames/ScaryMaze/criacao/cata_vento_scr.gd
@@ -1,16 +1,6 @@
 extends Area2D
 
-@export var velocity: float = 0.04
-## signal colided
-
-func _ready() -> void:
-	##mouse_entered.connect(_on_mouse_entered)
-	pass
-
-func _on_mouse_entered():
-	##colided.emit() 
-	##print("colococec")
-	pass
+@export var velocity: float = 1.0
 
 func _physics_process(delta: float) -> void:
-	rotate(velocity)
+	rotate(velocity * delta)
