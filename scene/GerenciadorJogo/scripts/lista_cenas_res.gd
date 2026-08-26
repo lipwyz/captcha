73325a2538b1e51483597da56a730ff37a7f1a92
Victor Cabral Maia <victorcabralmaia777@gmail.com>
2026-08-26 19:16:37 -0300

    Delete scene directory

--- scene/GerenciadorJogo/scripts/lista_cenas_res.gd
+++
@@ -1,4 +0,0 @@
-class_name ListaCenasRes
-extends Resource
-
-@export var lista_cenas: Array[PackedScene]
--- scene/MiniGames/LogoDVD/logo_dvd.gd
+++
@@ -1,7 +0,0 @@
-extends ConteudoAba
-
-@onready var mundo_dvd: DVDMundo = $SubViewportContainer/SubViewport/MundoDVD
-
-func _ready() -> void:
-	mundo_dvd.completo.connect(minigame_ganhar)
-	mundo_dvd.anuncio.connect(minigame_errar)
